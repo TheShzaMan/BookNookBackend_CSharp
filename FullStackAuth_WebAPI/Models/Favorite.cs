c0f5bcba069331c9c57d29927766c117dd8cb505
Shea Raymond <shea.raymond01@gmail.com>
2023-10-17 11:09:53 -0500

    Creating Favorite model

---
+++ FullStackAuth_WebAPI/Models/Favorite.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]  
+        public string BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string ThumbnailUrl { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+
+    }
+}
