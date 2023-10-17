8dff9634a926049f36e7241d6164074cdbbf752e
Shea Raymond <shea.raymond01@gmail.com>
2023-10-17 11:37:14 -0500

    Creating BookDetailsDto and ReviewWithUserDto

---
+++ FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using FullStackAuth_WebAPI.DataTransferObjects;
+
+
+
+namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class BookDetailsDto
+    {
+        public List<ReviewWithUserDTO> Reviews { get; set; }
+
+        public double AvgRating { get; set;}
+
+        public bool isFavorite {  get; set;}
+
+    }
+}
---
+++ FullStackAuth_WebAPI/DataTransferObjects/ReviewWithUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class ReviewWithUserDTO
+    {
+        public string UserName { get; set; }
+
+        public string Review {  get; set; }
+
+    }
+}
